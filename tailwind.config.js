28172e8f0979ca350482a9b7af9d5d6a92175316
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-01-15 21:18:59 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: {
+                    50: '#FDF8F6',
+                    100: '#E6DCD5',
+                    200: '#D4BFB4',
+                    300: '#C1A193',
+                    400: '#AE8472',
+                    500: '#9B6851',
+                    600: '#7D533F',
+                    700: '#5F3E2E',
+                    800: '#41291C',
+                    900: '#23150B',
+                },
+                secondary: {
+                    50: '#fdf8f6',
+                    100: '#f2e8e5',
+                    200: '#eaddd7',
+                    300: '#e0cec7',
+                    400: '#d2bab0',
+                    500: '#bfa094',
+                    600: '#a18072',
+                    700: '#977669',
+                    800: '#846358',
+                    900: '#43302b',
+                },
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+      ],
+    plugins: [forms],
+};
